f838281c1301af4e86ec471a5442e1278d3c8abe
elash1312 <lasherin1312@gmail.com>
2022-02-19 01:49:38 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,22 +7,26 @@ import React, { useState } from "react";
 const Home = () => {
 	const [selectedColor, setSelectedColor] = useState("yellow");
 	return (
-		<div className="traffic-light align-center">
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={
-					"light red" + (selectedColor === "red" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor("yellow")}
-				className={
-					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor("green")}
-				className={
-					"light green" + (selectedColor === "green" ? " glow" : "")
-				}></div>
+		<div className="thingy">
+			<div className="traffic-light align-center rounded">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor === "green" ? " glow" : "")
+					}></div>
+			</div>
 		</div>
 	);
 };
