7a087057952b1c0b343f35804141ee37dc0e915e
elash1312 <lasherin1312@gmail.com>
2022-02-19 00:22:05 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	// const ( selectedColor, getSelectedColor ) = useState("red");
+	const [selectedColor, getSelectedColor] = useState("red");
+	// setSelectedColor("blue");
 	return (
 		<div className="traffic-light">
-			<div className="light red glow"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
+			<div className={"light red glow" + ((selectedColor === "red") ? " glow" : ""}></div>
+			<div className={"light yellow" + ((selectedColor === "yellow") ? " glow" : ""}></div>
+			<div className={"light green" + ((selectedColor === "green") ? " glow" : ""}></div>
 		</div>
 	);
 };
