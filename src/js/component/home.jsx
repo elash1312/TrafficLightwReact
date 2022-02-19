112eccd4cef88dae17c0dcd602f5c307e54b35d6
elash1312 <lasherin1312@gmail.com>
2022-02-19 01:42:41 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,23 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState("red");
-
+	const [selectedColor, setSelectedColor] = useState("yellow");
 	return (
-		<div className="traffic-light">
+		<div className="traffic-light align-center">
 			<div
+				onClick={() => setSelectedColor("red")}
 				className={
-					"light red" + (selectedColor === "red" ? "glow" : "")
+					"light red" + (selectedColor === "red" ? " glow" : "")
 				}></div>
 			<div
+				onClick={() => setSelectedColor("yellow")}
 				className={
-					"light yellow" + (selectedColor === "yellow" ? "glow" : "")
+					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
 				}></div>
 			<div
+				onClick={() => setSelectedColor("green")}
 				className={
-					"light green" + (selectedColor === "green" ? "glow" : "")
+					"light green" + (selectedColor === "green" ? " glow" : "")
 				}></div>
 		</div>
 	);
